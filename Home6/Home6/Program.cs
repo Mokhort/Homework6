cfde5a4afa7bd5bbaf52940d044349e5dd99fc1d
Mpkhort <giselle98@mail.ru>
2021-06-08 23:54:26 +0300

    add code

---
+++ Home6/Home6/Program.cs
@@ -0,0 +1,63 @@
+﻿using Homework3;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Home6
+{
+    class Program
+    {
+        private static TcpListener listener;
+        private const int serverPort = 2365;
+        private static bool flag;
+        static Context context = new Context();
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Server start");
+            listener = new TcpListener(IPAddress.Any, serverPort);
+            flag = true;
+            context.Database.EnsureCreated();
+            await Listen();
+        }
+        private static async Task Listen()
+        {
+            listener.Start();
+            TcpClient client = await listener.AcceptTcpClientAsync();
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+
+            string msg = await reader.ReadLineAsync();
+            string[] words = msg.Split(",");
+            Console.WriteLine($"Message from client: name = {words[0]} lastname = {words[1]} email = {words[2]} password = {words[3]} passport = {words[4]}");
+            IDbContextTransaction transaction = await context.Database.BeginTransactionAsync();
+            try
+            {
+
+                context.Passanger.Add(new Passanger()
+                {
+                    Name = words[0],
+                    LastName = words[1],
+                    Email = words[2],
+                    Password = words[3],
+                    Passport = words[4]
+                });
+                await transaction.CommitAsync();
+                await context.SaveChangesAsync();
+                await writer.WriteLineAsync("Your registration ended successfully");
+                await writer.FlushAsync();
+            }
+            catch
+            {
+                await writer.WriteLineAsync("FAILED REGISTRATION");
+            }
+            listener.Stop();
+            client.Close();
+        }
+    }
+}
---
+++ Home6_client/Home6_client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Home6_client
+{
+    class Program
+    {
+        private const int serverPort = 2365;
+        private static TcpClient client;
+        static async Task Main(string[] args)
+        {
+            string cmd;
+            string answer;
+            bool flag = true;
+            string message;
+            client = new TcpClient();
+            await client.ConnectAsync(IPAddress.Loopback, serverPort);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            
+
+            while (flag)
+            {
+                Console.WriteLine("Registration - 1 Exit - 0 ");
+                cmd = Console.ReadLine();
+                if (cmd == "1") {
+                    Console.Write("Enter name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter last name: ");
+                    string lastname = Console.ReadLine();
+                    Console.Write("Enter email: ");
+                    string email = Console.ReadLine();
+                    Console.Write("Enter password: ");
+                    string password = Console.ReadLine();
+                    Console.Write("Enter passport: ");
+                    string passport = Console.ReadLine();
+                    message = name + ','+ lastname+ ',' + email + ',' + password + ',' + passport;
+                    Console.WriteLine(message);
+                    await writer.WriteLineAsync(message);
+                    await writer.FlushAsync();
+                    answer = await reader.ReadLineAsync();
+                    Console.WriteLine(answer);
+                }
+                else if (cmd == "0")
+                {
+                    flag = false;
+                }
+                else Console.WriteLine("Invalid value");
+
+            }
+        }
+    }
+}
